7dcd152bde68d0cd9374304bc5992f3348c65929
Sumit Chohan <chauhansumit@gmail.com>
2018-09-18 09:26:07 -0500

    wip

---
+++ downloadmodel.sh
@@ -0,0 +1,3 @@
+wget http://storage.googleapis.com/download.tensorflow.org/models/object_detection/faster_rcnn_resnet101_coco_11_06_2017.tar.gz
+tar -xvf faster_rcnn_resnet101_coco_11_06_2017.tar.gz
+mv faster_rcnn_resnet101_coco_11_06_2017/model.ckpt.* .
